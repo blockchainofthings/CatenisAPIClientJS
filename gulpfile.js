b4cd4cb47188f854793bc407220aaaa64a9cc454
Cláudio Sá de Castro <claudio@hiades.com.br>
2019-08-15 15:42:11 -0300

    Enhance mechanism used to execute test cases making it more flexible.

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,17 @@ var gulp = require('gulp');
 var jasmineBrowser = require('gulp-jasmine-browser');
 
 gulp.task('jasmine-chrome', function() {
-    return gulp.src(['test/helpers/*.js', 'build/CatenisAPIClientJS.min.js', 'test/**/*Spec.js'])
-    .pipe(jasmineBrowser.specRunner({
-        console: true
-    }))
-    .pipe(jasmineBrowser.headless({
-        catch: true,
-        driver: 'chrome',
-        throwFailures: false
-    }));
-});
+    var testSpecs = '**/*Spec.js';
 
-gulp.task('jasmine-chrome-compress', function() {
-    return gulp.src(['test/helpers/*.js', 'build/CatenisAPIClientJS.min.js', 'test/TestDataCompressionSpec.js'])
+    if (process.env.TEST_SPECS) {
+        testSpecs = process.env.TEST_SPECS;
+
+        if (process.env.npm_package_version) {
+            testSpecs = testSpecs.replace('{!npm_package_version}', process.env.npm_package_version);
+        }
+    }
+
+    return gulp.src(['test/helpers/*.js', 'build/CatenisAPIClientJS.min.js', 'test/' + testSpecs])
     .pipe(jasmineBrowser.specRunner({
         console: true
     }))
--- test/TestChangesToVer2.2.0Spec.js
+++ test/TestChangesToVer2.2.0Spec.js
@@ -1,4 +1,4 @@
-xdescribe('Test changes to Catenis API client ver. 2.2.0.', function  () {
+describe('Test changes to Catenis API client ver. 2.2.0.', function  () {
     var device1 = {
         id: 'd8YpQ7jgPBJEkBrnvp58'
     };
